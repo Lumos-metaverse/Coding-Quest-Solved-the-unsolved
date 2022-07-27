5e2047ca7dbf19e87a10751260c6029c678b503d
hnishal <hnishal12799@gmail.com>
2022-07-27 23:06:37 +0530

    fixed errors

--- erc20.sol
+++ erc20.sol
@@ -1,113 +1,167 @@
 pragma solidity ^0.4.24;
- 
+
 //Safe Math Interface
- 
+
 contract SafeMath {
- 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
+    function safeAdd(uint256 a, uint256 b) public pure returns (uint256 c) {
         c = a + b;
         require(c >= a);
     }
- 
-    function safeSub(uint a, uint b) public pure returns (uint c) {
+
+    function safeSub(uint256 a, uint256 b) public pure returns (uint256 c) {
         require(b <= a);
         c = a - b;
     }
- 
-    function safeMul(uint a, uint b) public pure returns (uint c) {
+
+    function safeMul(uint256 a, uint256 b) public pure returns (uint256 c) {
         c = a * b;
         require(a == 0 || c / a == b);
     }
- 
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
+
+    function safeDiv(uint256 a, uint256 b) public pure returns (uint256 c) {
         require(b > 0);
         c = a / b;
     }
 }
- 
- 
+
 //ERC Token Standard #20 Interface
- 
+
 contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
- 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    function totalSupply() public constant returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        public
+        constant
+        returns (uint256 balance);
+
+    function allowance(address tokenOwner, address spender)
+        public
+        constant
+        returns (uint256 remaining);
+
+    function transfer(address to, uint256 tokens) public returns (bool success);
+
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
 }
- 
- 
+
 //Contract function to receive approval and execute function in one call
- 
+
 contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+    function receiveApproval(
+        address from,
+        uint256 tokens,
+        address token,
+        bytes data
+    ) public;
 }
- 
+
 //Actual token contract
- 
+
 contract QKCToken is ERC20Interface, SafeMath {
     string public symbol;
-    string public  name;
+    string public name;
     uint8 public decimals;
-    uint public _totalSupply;
- 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
- 
+    uint256 public _totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
     constructor() public {
         symbol = "QKC";
         name = "QuikNode Coin";
         decimals = 2;
         _totalSupply = 100000;
-        balances[0xDf2AC80661DAB9010f9B9cc88e43f3d2E1035fF4] = _totalSupply;
-        emit Transfer(address(0), 0xDf2AC80661DAB9010f9B9cc88e43f3d2E1035fF4, _totalSupply);
+        balances[0x37c4677d88dD29dD603E368E2d0B6308573B5280] = _totalSupply;
+        emit Transfer(
+            address(0),
+            0x37c4677d88dD29dD603E368E2d0B6308573B5280,
+            _totalSupply
+        );
     }
- 
-    function totalSupply() public constant returns (uint) {
-        return _totalSupply  - balances[address(0)];
+
+    function totalSupply() public constant returns (uint256) {
+        return _totalSupply - balances[address(0)];
     }
- 
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+
+    function balanceOf(address tokenOwner)
+        public
+        constant
+        returns (uint256 balance)
+    {
         return balances[tokenOwner];
     }
- 
-    function transfer(address to, uint tokens) public returns (bool success) {
+
+    function transfer(address to, uint256 tokens)
+        public
+        returns (bool success)
+    {
         balances[msg.sender] = safeSub(balances[msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(msg.sender, to, tokens);
         return true;
     }
- 
-    function approve(address spender, uint tokens) public returns (bool success) {
+
+    function approve(address spender, uint256 tokens)
+        public
+        returns (bool success)
+    {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
- 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public returns (bool success) {
         balances[from] = safeSub(balances[from], tokens);
         allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
         balances[to] = safeAdd(balances[to], tokens);
         emit Transfer(from, to, tokens);
         return true;
     }
- 
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+
+    function allowance(address tokenOwner, address spender)
+        public
+        constant
+        returns (uint256 remaining)
+    {
         return allowed[tokenOwner][spender];
     }
- 
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+
+    function approveAndCall(
+        address spender,
+        uint256 tokens,
+        bytes data
+    ) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        ApproveAndCallFallBack(spender).receiveApproval(
+            msg.sender,
+            tokens,
+            this,
+            data
+        );
         return true;
     }
- 
-    function () public payable {
+
+    function() public payable {
         revert();
     }
 }
